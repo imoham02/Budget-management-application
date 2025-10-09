15357ce36ad473866606f20d8a7712f6202e82db
Ismail Mohamed <ismailmoham02@gmail.com>
2025-10-09 14:45:52 +0100

    Added scanner class to get user input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! What is your full name?");
+        String name = scanner.nextLine();
+        double balance = 1000.00;
+        System.out.println("Welcome to FinCore CLI Banking!");
+        System.out.println("Account Holder: " + name);
+        System.out.println("Initial balance: £" + String.format("%.2f", balance));
     }
 }
