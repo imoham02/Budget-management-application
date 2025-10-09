d264271b9b4a0979fd31ab2b1696f61761928401
Ismail Mohamed <ismailmoham02@gmail.com>
2025-10-09 15:51:15 +0100

    Added the logic to enter and exit the banking service

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,5 +9,42 @@ public class Main {
         System.out.println("Welcome to FinCore CLI Banking!");
         System.out.println("Account Holder: " + name);
         System.out.println("Initial balance: £" + String.format("%.2f", balance));
+        boolean hasExited = false;
+        while (!hasExited) {
+            String bankTextBlock = """
+                    
+                    === Fincore CLI Banking Menu ===
+                    1. Deposit
+                    2. Withdraw
+                    3. Check Balance
+                    4. Exit
+                    """;
+            System.out.println(bankTextBlock);
+            System.out.println("Please select an option (1-4):");
+            int option = 0;
+            boolean validOption = false;
+            while (!validOption) {
+                try {
+                    option = Integer.parseInt(scanner.nextLine());
+                    if (option < 1 || option > 4) {
+                        System.out.println("Invalid choice. Please select again! (1-4):");
+                    } else {
+                        validOption = true;
+                    }
+                } catch (NumberFormatException badUserOption) {
+                    System.out.println("Please enter a valid number(1-4):");
+                }
+            }
+            switch (option) {
+                case 1, 2, 3 -> System.out.println();
+                case 4 -> {
+                    System.out.println("Thank you for using FinCore CLI Banking. Goodbye " + name + "!");
+                    hasExited = true;
+                }
+
+            }
+        }
+
+        scanner.close();
     }
 }
