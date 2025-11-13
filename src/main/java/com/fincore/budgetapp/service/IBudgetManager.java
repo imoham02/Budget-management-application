018bf315268cfbbd834a27962a41746d249756d5
Ismail Mohamed <ismailmoham02@gmail.com>
2025-11-13 15:39:47 +0000

    Unit tests for BudgetManager class

--- src/main/java/com/fincore/budgetapp/service/IBudgetManager.java
+++ src/main/java/com/fincore/budgetapp/service/IBudgetManager.java
@@ -1,7 +1,16 @@
 package com.fincore.budgetapp.service;
 
+import com.fincore.budgetapp.model.BudgetEntry;
+import com.fincore.budgetapp.model.Transaction;
+
+import java.util.List;
+
 public interface IBudgetManager {
     void deposit();
     void withdraw();
     void checkBalance();
+    void listTransactions(List<Transaction> transactionList);
+    void filterTransactions(int option);
+    void sortTransactions(List<Transaction> transactionList);
+    BudgetEntry getCurrentAccount();
 }
--- src/test/java/com/fincore/budgetapp/service/BudgetManagerTest.java
+++ src/test/java/com/fincore/budgetapp/service/BudgetManagerTest.java
@@ -1,6 +1,10 @@
 package com.fincore.budgetapp.service;
 
+import com.fincore.budgetapp.model.BudgetEntry;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,6 +16,50 @@ class BudgetManagerTest {
     public void setup() {
         budgetManager = new BudgetManager();
     }
+
+    @Nested
+    @DisplayName("Login/Logout functionality tests")
+    class Login {
+        @Test
+        @DisplayName("Should create a new account for new user")
+        void shouldCreateANewAccountForNewUser() {
+            budgetManager.loginToBudgetAccount("Test User");
+            assertNotNull(budgetManager.getCurrentAccount());
+            assertEquals("Test User", budgetManager.getCurrentAccount().getUser().name());
+        }
+
+        @Test
+        @DisplayName("Should retrieve existing account if account exists")
+        void shouldRetrieveExistingUserIfExists() {
+            budgetManager.loginToBudgetAccount("test");
+            BudgetEntry loginOne = budgetManager.getCurrentAccount();
+            budgetManager.logoutOfBudgetAccount();
+            budgetManager.loginToBudgetAccount("test");
+            BudgetEntry loginTwo = budgetManager.getCurrentAccount();
+            assertSame(loginOne, loginTwo);
+        }
+
+        @Test
+        @DisplayName("Should be case-insensitive when retrieving existing account if account exists")
+        void shouldBeCaseInsensitiveWhenRetrievingExistingUserIfExists() {
+            budgetManager.loginToBudgetAccount("test");
+            budgetManager.logoutOfBudgetAccount();
+            budgetManager.loginToBudgetAccount("TeSt");
+            assertNotNull(budgetManager.getCurrentAccount());
+            assertEquals("test", budgetManager.getCurrentAccount().getUser().name());
+        }
+
+        @Test
+        @DisplayName("Should set current account to null after logout")
+        void shouldSetCurrentAccountToNullAfterLogout() {
+            budgetManager.loginToBudgetAccount("test");
+            assertNotNull(budgetManager.getCurrentAccount());
+            budgetManager.logoutOfBudgetAccount();
+            assertNull(budgetManager.getCurrentAccount());
+        }
+    }
+
+    // I am not sure how to test the other methods as they require the Console class and user input
 
 
 }
